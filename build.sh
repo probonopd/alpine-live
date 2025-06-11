91ee20bd62cf17dba508c846b2c90128cee86d30
probonopd <probonopd@users.noreply.github.com>
2025-06-11 19:32:23 +0200

    --profile=TeaISO/profiles/alpine

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ sudo apt install xorriso grub-pc-bin grub-efi mtools make python3 \
     gcc wget curl unzip xz-utils zstd -y
 make && make install
 cd ../
-mkteaiso --profile=$(pwd) --output=/output/ --debug 2>&1
+mkteaiso --profile=TeaISO/profiles/alpine --output=/output/ --debug 2>&1
